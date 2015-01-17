4084aa74836660df9fe8149a8d6205c0a43957cd
Mike <luzymike@gmail.com>
2015-01-17 17:34:25 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -50,10 +50,10 @@ static const int GRID_COLUMNS = 10;
             Creature *creature = [[Creature alloc] initCreature];
             creature.anchorPoint = ccp(0, 0);
             creature.position = ccp(x, y);
-            //[self addChild:creature];
+            [self addChild:creature];
             
             // this is shorthand to access an array inside an array
-            _gridArray[i][j] = creature;
+            //_gridArray[i][j] = creature;
             
             // make creatures visible to test this method, remove this once we know we have filled the grid properly
             creature.isAlive = YES;
