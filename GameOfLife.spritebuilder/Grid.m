e2d8e6c49916dc0745aa8fd12f5713d957e8a42d
Mike <luzymike@gmail.com>
2015-01-17 17:19:09 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -48,7 +48,7 @@ static const int GRID_COLUMNS = 10;
         
         for (int j = 0; j < GRID_COLUMNS; j++) {
             Creature *creature = [[Creature alloc] initCreature];
-            creature.anchorPoint = ccp(0, 0);
+            creature.anchorPoint = ccp(0.5, 0.5);
             creature.position = ccp(x, y);
             [self addChild:creature];
             
